9296bf462509e2a0eb09f78268b90206982fda06
el <elarasu@gmail.com>
2015-08-07 22:00:01 -0700

    updated comments

--- version/version.go
+++ version/version.go
@@ -1,7 +1,9 @@
 /*
- * Copyright(c) 2015 elarasu. All rights reserved
- * Licensed under the Apache License, Version 2.0 (the "License");
- */
+ Copyright (c) 2015 elarasu. All rights reserved.
+ Licensed under the Apache License, Version 2.0 (the "License");
+*/
+
+// version - first step towards semver package naming.
 package version
 
 import (
@@ -20,7 +22,7 @@ type Version struct {
 const SEPARATOR string = "."
 const BASE10 int = 10
 
-// returns human friendly version major.minor.patch
+// human friendly version string - major.minor.patch
 func (v Version) String() string {
 	return strings.Join(([]string{
 		strconv.FormatInt(v.Major, BASE10),
@@ -29,8 +31,7 @@ func (v Version) String() string {
 		SEPARATOR)
 }
 
-// returns command line friendly version with
-// appname version and go version
+// command line friendly version string with appname and go runtime version
 func (v Version) AppString(appName string) string {
 	return fmt.Sprintf("%s v%s (built w/%s)", appName, v.String(), runtime.Version())
 }
